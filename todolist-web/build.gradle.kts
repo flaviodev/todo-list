edb579d50f6837e7e35bf0bd8f9d230d361363d8
Flávio de Souza <fdsdev@gmail.com>
2020-02-07 22:20:39 -0300

    Update build.gradle.kts

--- todolist-web/build.gradle.kts
+++ todolist-web/build.gradle.kts
@@ -1,27 +1,35 @@
-val logback_version: String by project
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
-val kotlin_version: String by project
+val mockk_version: String by project
+val koin_version: String by project
 
 plugins {
     application
 }
 
+
 application {
-    mainClassName = "io.ktor.server.cio.EngineMain"
+    mainClassName = "io.ktor.server.netty.EngineMain"
 }
 
 repositories {
-    mavenLocal()
-    jcenter()
     maven { url = uri("https://kotlin.bintray.com/ktor") }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
-    implementation("io.ktor:ktor-server-cio:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-mustache:$ktor_version")
+    implementation("org.koin:koin-ktor:$koin_version")
+
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("io.mockk:mockk:$mockk_version")
 }
