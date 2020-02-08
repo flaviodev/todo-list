14bd6ce82d76bb3856d404f78f4a8932ffa363ee
Flávio de Souza <fdsdev@gmail.com>
2020-02-08 00:34:03 -0300

    www contents

--- todolist-web/src/main/kotlin/web/Application.kt
+++ todolist-web/src/main/kotlin/web/Application.kt
@@ -1,4 +1,6 @@
-package web
+@file:Suppress("PackageDirectoryMismatch")
+
+package com.rocksolidknowledge.todolist.web
 
 import com.github.mustachejava.DefaultMustacheFactory
 import io.ktor.application.Application
@@ -14,36 +16,41 @@ import io.ktor.routing.Routing
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.sessions.SessionStorageMemory
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
+
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, commandLineEnvironment(args)).start()
 }
 
-@SuppressWarnings("unused")
+
+@Suppress("unused") // Referenced in application.conf
 fun Application.module() {
+
     install(StatusPages) {
         when {
             isDev -> {
-                this.exception<Throwable> {e ->
+                this.exception<Throwable> { e ->
                     call.respondText(e.localizedMessage, ContentType.Text.Plain, HttpStatusCode.InternalServerError)
                     throw e
                 }
             }
 
             isTest -> {
-                this.exception<Throwable> {e ->
+                this.exception<Throwable> { e ->
                     call.response.status(HttpStatusCode.InternalServerError)
                     throw e
                 }
             }
 
             isProd -> {
-                this.exception<Throwable> {e ->
+                this.exception<Throwable> { e ->
                     call.respondText(e.localizedMessage, ContentType.Text.Plain, HttpStatusCode.InternalServerError)
                     throw e
                 }
             }
-
         }
     }
 
@@ -51,14 +58,20 @@ fun Application.module() {
         mustacheFactory = DefaultMustacheFactory("templates")
     }
 
+
     install(Routing) {
-        if(isDev) trace {
+        if (isDev) trace {
             application.log.trace(it.buildText())
         }
 
+        todos()
         staticResources()
     }
+
 }
+
+data class UserSession(val name: String)
+
 
 val Application.envKind get() = environment.config.property("ktor.environment").getString()
 val Application.isDev get() = envKind == "dev"
